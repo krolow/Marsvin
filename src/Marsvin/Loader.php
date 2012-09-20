e54a088591cf6e0b0eedcdfcbef4ccc21a55430a
Vinícius Krolow <krolow@gmail.com>
2012-09-20 14:39:59 -0300

    Removing factory putting that inside the loader

--- src/Marsvin/Loader.php
+++ src/Marsvin/Loader.php
@@ -1,15 +1,36 @@
 <?php
+namespace Marsvin;
+
 class Loader
 {
     
-    public function __construct()
-    {
+	private $providerClass;
 
+    public function __construct($providerClass)
+    {
+    	$this->providerClass = $providerClass;
     }
 
     public function load()
     {
-        
+       if (!class_exists($this->providerClass)) {
+       		throw new \InvalidArgumentException(
+       			sprintf(
+       				'It was not possible to load the given provider class %s',
+       				$this->providerClass
+       			)
+       		);
+        }
+
+        $provider = new $class($container);
+
+        if ($provider instanceof Marsvin\Provider\ProviderInterface) {
+        	return $provider;
+        }
+
+        throw new \InvalidArgumentException(
+        	sprintf('The given provider must implements the Marsvin\Provider\ProviderInterface')
+        );
     }
 
 }
