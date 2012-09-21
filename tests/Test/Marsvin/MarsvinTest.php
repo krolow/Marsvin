267bf669ecc089692f0111d4d974c24a94152db6
krolow <krolow@gmail.com>
2012-09-21 17:49:59 -0300

    Creating the basic structure of tests

---
+++ tests/Test/Marsvin/MarsvinTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Test\Marsvin;
+
+use Marsvin\Marsvin;
+
+class MarsvinTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $marsvin;
+
+    protected function setup()
+    {
+        $this->event    = $this->getMock('Evenement\EventEmmiter');
+        $this->process  = $this->getMock('Spork\ProcessManager');
+        $this->provider = $this->getMock('Marsvin\Provider\Provider');
+        $this->marsvin = new Marsvin($this->event, $this->process, $this->provider);
+    }
+
+    protected function tearDown()
+    {
+        $this->marsvin = null;
+    }
+
+    public function testBasicMethods()
+    {
+        $this->marsvin->request(function (RequesterInterface $request) {
+            $data = $request->getAdapter()->request('http://google.com');
+
+            return new Response($data);
+        })->parse(function (ParserInterface $parser, ResponseInterface $response) {
+            $data = $parse->getAdapter()->parse($response->get());
+
+            return new Response($data);
+        })->persist(function (PersisterInterface $persister, ResponseInterface $response) {
+            $entities = $response->get();
+            foreach ($entitis as $entity) {
+                $persister->getAdapter()->persist($entity);
+            }
+            $persister->getAdapter()->flush();
+        });
+    }
+
+}
