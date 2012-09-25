029c0663e6045617b1a1a095711b61186daa0374
krolow <krolow@gmail.com>
2012-09-25 10:08:33 -0300

    Fixing sintax mistake in the process method

--- src/Marsvin/AbstractLayer.php
+++ src/Marsvin/AbstractLayer.php
@@ -1,5 +1,5 @@
 <?php
-namespace Marsvin\AbstractParser;
+namespace Marsvin;
 
 use Evenement\EventEmitter;
 use Spork\ProcessManager;
@@ -7,63 +7,68 @@ use Spork\ProcessManager;
 abstract class AbstractLayer
 {
 
-	protected $event;
+    protected $event;
 
-	protected $process;
+    protected $process;
 
-	protected $adapter;
+    protected $adapter;
 
-	protected $eventName;
-	
-	public function __construct(EventEmitter $event, ProcessManager $process, AdapterInterface $adapter)
-	{
-		$this->event = $event;
-		$this->process = $process;
-		$this->adapter = $adapter;
-	}
+    protected $eventName;
 
-	public function setEvent
+    public function __construct(EventEmitter $event, ProcessManager $process, AdapterInterface $adapter)
+    {
+        $this->event   = $event;
+        $this->process = $process;
+        $this->adapter = $adapter;
+    }
 
-	public function getEvent($event)
-	{
-		return $this->event;
-	}
+    public function getEvent($event)
+    {
+        return $this->event;
+    }
 
-	public function getProcess($process)
-	{
-		return $this->process;
-	}
+    public function getProcess($process)
+    {
+        return $this->process;
+    }
 
-	public function getAdapter()
-	{
-		return $this->adapter;
-	}
+    public function getAdapter()
+    {
+        return $this->adapter;
+    }
 
-	public function setEventName($eventName)
-	{
-		$this->eventName = $eventName;
-	}
+    public function setEventName($eventName)
+    {
+        $this->eventName = $eventName;
+    }
 
-	public function getEventName()
-	{
-		if (empty($this->eventName)) {
-			if (!isset(static::EVENT_NAME)) {
-				throw \RuntimeException('Your must have set one event name');
-			}
+    public function getEventName()
+    {
+        if (empty($this->eventName)) {
+            if (!static::EVENT_NAME) {
+                throw \RuntimeException('Your must have set one event name');
+            }
 
-			return static::EVENT_NAME;
-		}
+            return static::EVENT_NAME;
+        }
 
-		return $this->eventName;
-	}
+        return $this->eventName;
+    }
 
-	public function process($process)
-	{
-		$this->process
-			->fork($process)
-			->then(function (Spork\Fork $fork) {
-				$this->event->emit($this->getEventName(), array($fork->getResult());
-			});
-	}
+    public function process($process)
+    {
+        $self = $this;
+
+        $this->process
+            ->fork($process)
+            ->then(
+                function (Spork\Fork $fork) use ($self) {
+                    $self->event->emit(
+                        $self->getEventName(), 
+                        array($fork->getResult())
+                    );
+                }
+            );
+    }
 
 }
