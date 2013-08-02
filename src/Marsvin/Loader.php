f085a6ee19a5064b51e82be05d738a73dab7e2d9
Vinícius Krolow <krolow@gmail.com>
2013-08-02 02:26:46 -0300

    Adding LICENSE note in files

--- src/Marsvin/Loader.php
+++ src/Marsvin/Loader.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Marsvin package.
+ *
+ * (c) Vinícius Krolow <krolow@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Marsvin;
 
 use Symfony\Component\Console\Helper\HelperSet;
